e7b3aab453aaefe80a36037a014738f77612471b
Ibnu Ali <ibnuali@gmail.com>
2023-03-14 14:57:59 +0700

    remove method unused

--- src/utils/apiFeatures.js
+++ src/utils/apiFeatures.js
@@ -4,27 +4,6 @@ class APIFeatures {
     this.queryString = queryString;
   }
 
-  sort() {
-    if (this.queryString.sort) {
-      const sortBy = this.queryString.sort.split(',').join(' ');
-      this.query = this.query.sort(sortBy);
-    } else {
-      this.query = this.query.sort('-createdAt');
-    }
-
-    return this;
-  }
-
-  limitFields() {
-    if (this.queryString.fields) {
-      const fields = this.queryString.fields.split(',').join(' ');
-      this.query = this.query.select(fields);
-    } else {
-      this.query = this.query.select('-__v');
-    }
-    return this;
-  }
-
   paginate() {
     const page = this.queryString.page * 1 || 1;
     const limit = this.queryString.limit * 1 || 10;
