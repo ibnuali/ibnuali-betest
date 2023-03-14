38bf696047a4cd4fa6b94c9dfbe8b9cc0776c585
ibnuali <ibnuali6@gmail.com>
2023-03-14 23:04:30 +0700

    add route first index

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const app = require('./src/app');
 db.connect();
 
 const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.json({ message: 'ms-ibnuali-betest' });
+});
+
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
