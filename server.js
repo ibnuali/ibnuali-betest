52245111558dc4542a6d13cf3c49f644214326bf
ibnuali <ibnuali6@gmail.com>
2023-03-14 22:32:58 +0700

    port exclude env

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = require('./src/app');
 
 db.connect();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
